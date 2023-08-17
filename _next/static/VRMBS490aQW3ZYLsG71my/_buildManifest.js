6dc6403fbcd3e9b114e27d946bcac66a4f887e4f
s1f102103707 <s1f102103707@users.noreply.github.com>
2023-08-17 13:48:05 +0000

    deploy: 6214bca4f3d8d93c6a1baee702fb9302d9f53864

--- _next/static/VRMBS490aQW3ZYLsG71my/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/25a018b0c590a9d7.css","static/chunks/pages/index-57889d92ec9ae02b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
